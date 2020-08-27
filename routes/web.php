019534f3bb2c2b8f02d7e3d8809cf1c6d658c890
DanielMigchels <danielm12@hotmail.nl>
2020-08-27 13:12:06 +0200

    Add Link Submit Form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +19,22 @@ Route::get('/', function () {
     $links = \App\Link::all();
 
     return view('welcome', ['links' => $links]);
+});
+
+Route::get('/submit', function () {
+    return view('submit');
+});
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = tap(new App\Link($data))->save();
+
+    return redirect('/');
 });
 
 Auth::routes();
